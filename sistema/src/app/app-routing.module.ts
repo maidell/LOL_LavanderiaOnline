ea420c685189de07421cf345b1218c53cc905b6b
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-08-14 04:10:49 -0300

    :construction: Finish #1 - without bootstrap, just HTML & CSS

--- sistema/src/app/app-routing.module.ts
+++ sistema/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutocadastroComponent } from './autocadastro/autocadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'autocadastro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'autocadastro',
+    component: AutocadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
