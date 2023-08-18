cd4dce61f6a66f25f87ccff6b5a28448d40f06e5
Ariane Souza <ariane.souza.prime@segurosunimed.com.br>
2023-08-18 14:43:07 -0300

    Inserindo menu lateral na tela inicial

--- sistema/src/app/app.module.ts
+++ sistema/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+import { DashboardComponent } from './cliente/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutocadastroComponent
+    AutocadastroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
