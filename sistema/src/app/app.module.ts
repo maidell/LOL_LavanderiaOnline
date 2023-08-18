156fb8375a3ee4db11ed3ea6f92d2fa833befb2d
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2023-08-18 03:48:27 -0300

    Criada tela de login

--- sistema/src/app/app.module.ts
+++ sistema/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutocadastroComponent
+    AutocadastroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
