14ff37e0da949db4555223d45d4ef2fcda6b8b61
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-08-18 17:43:31 -0300

    :construction: Fix component cliente and routes  issue #3  done

--- sistema/src/app/app-routing.module.ts
+++ sistema/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutocadastroComponent } from './autocadastro/autocadastro.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login',component: LoginComponent},
-  { path: 'autocadastro', component: AutocadastroComponent}
+  { path: 'autocadastro', component: AutocadastroComponent},
+  { path: 'dashboard', component: DashboardComponent}  
 ];
 
 @NgModule({
