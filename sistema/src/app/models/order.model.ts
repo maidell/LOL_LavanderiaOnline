d36ff5dabe3e9e3dd0af5b51e0aeb941631621df
arianessouza <ariane.souza@ufpr.br>
2023-08-23 20:17:09 -0300

    RF005 Criando models Order e Clothing

---
+++ sistema/src/app/models/order.model.ts
@@ -0,0 +1,15 @@
+import { Clothing } from "./clothing.model";
+
+export class Order {
+    id : number = 0;
+    time : number = 0;
+    status : string = 'Em Aberto';
+    value : number = 0;
+    clothings : Clothing[] = [];
+
+    constructor(time: number, value: number) {
+        this.id = Math.floor(Math.random() * 1000) + 1;
+        this.time = time;
+        this.value = value;
+    }
+}
