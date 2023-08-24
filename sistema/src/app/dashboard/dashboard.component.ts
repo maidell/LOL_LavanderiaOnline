a7f0ea4a121774f9d1d8f4f03ba4e6d63affa8a2
arianessouza <ariane.souza@ufpr.br>
2023-08-24 00:48:57 -0300

    RF005 Apresentação dos pedidos na tela de dashboard

--- sistema/src/app/dashboard/dashboard.component.ts
+++ sistema/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services';
+import { Order } from '../models/order.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  
+  constructor(private orderService: OrderService){ }
+  
+  listOrder: Order[] = this.orderService.listOrder
+
+  ngOnInit(): void{ }
 }
