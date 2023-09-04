b5147a8e51bce71d3ab6b7a771dfd8eda7c5c87f
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-09-04 10:40:34 -0300

    :construction: Fix dashboard funcionario
    Arrumei a dashboard do funcionario, corrigi as cores dos status e implementei o metodo de rejeitar pedido

--- sistema/src/app/cliente/pedido/listarPedido/listarPedido.component.ts
+++ sistema/src/app/cliente/pedido/listarPedido/listarPedido.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/order.model';
 import { OrderService } from 'src/app/services/order.service';
-
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-listar-pedido',
   templateUrl: './listarPedido.component.html',
   styleUrls: ['./listarPedido.component.scss']
 })
-export class ListarPedidoComponent {
-  constructor(public orderService: OrderService) { }
+export class ListarPedidoComponent implements OnInit{
+  constructor(public orderService: OrderService, private authService: AuthService) { }
   listOrder: Order[] = this.orderService.listOrder;
+  isEmployee: boolean = false;
   order: any;
+
+  ngOnInit(): void {
+
+    this.listOrder = this.orderService.listOrder;
+    this.isEmployee = !!this.authService.isEmployee();
+  }
 
   pagarPedido(order: Order): void {
     console.log(order);
@@ -25,4 +32,9 @@ export class ListarPedidoComponent {
     alert(`Pedido Cancelado!\nNúmero de Pedido: ${order.id}`);
   }
 
+  recolherPedido(order: Order): void {
+    order.status = 'Recolhido';
+    this.orderService.updateOrder(order);
+    alert(`Pedido Recolhido!\nNúmero de Pedido: ${order.id}`);
+  }
 }
