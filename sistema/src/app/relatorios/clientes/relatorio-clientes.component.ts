b01412198d518c911df35a642c6fb1277ad21318
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-09-28 21:07:09 -0300

    relatorio de clientes

--- sistema/src/app/relatorios/clientes/relatorio-clientes.component.ts
+++ sistema/src/app/relatorios/clientes/relatorio-clientes.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+import { User } from 'src/app/models';
+import { AuthService } from 'src/app/services';
 
 @Component({
     selector: 'app-relatorio-clientes',
     templateUrl: './relatorio-clientes.component.html',
     styleUrls: ['./relatorio-clientes.component.scss']
 })
+export class RelatorioClientesComponent implements OnInit {
+    clientes: User[] = [];
 
-export class RelatorioClientesComponent { }
+    constructor(private authService: AuthService) { }
+
+    ngOnInit(): void {
+        this.clientes = this.authService.getUsersByRole('cliente');
+    }
+
+    generatePDF() {
+        const doc = new jsPDF();
+
+        doc.text('Relatório de Clientes', 10, 10);
+
+        const data = this.clientes.map(cliente => [
+            cliente.name,
+            cliente.email,
+            cliente.cpf,
+        ]);
+
+        const columns = ['Nome', 'Email', 'CPF'];
+
+        // @ts-ignore
+        doc.autoTable({
+            head: [columns],
+            body: data,
+            startY: 20
+        });
+
+        doc.save('relatorio_clientes.pdf');
+    }
+}
