31b791e25d8faa27b72f190f4ac2a3cad11d5594
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-10-06 21:07:02 -0300

    gerar relatorio de receitas

--- sistema/src/app/app.module.ts
+++ sistema/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from "@angular/router";
-import {NgxPrintModule} from 'ngx-print';
+import { NgxPrintModule } from 'ngx-print';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
