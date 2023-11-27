f97d3aadad764fdd1bd2f3418bab8a9aef2984a9
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-11-27 01:15:04 -0300

    editar roupa

--- sistema/src/app/roupas/roupas.module.ts
+++ sistema/src/app/roupas/roupas.module.ts
@@ -5,7 +5,7 @@ import { ListarRoupaComponent } from './listar-roupa/listar-roupa.component';
 import { EditarRoupaComponent } from './editar-roupa/editar-roupa.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class RoupasModule { }
